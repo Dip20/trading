1625698b6db0b5ce68b20ba5357662723bbc4acd
Dip20 <info.dip.sarkar@gmail.com>
2021-04-14 01:00:34 +0530

    contact us section created, scroll up btn created

---
+++ vendor/js/slider.js
@@ -0,0 +1,24 @@
+// slider js
+   $('.owl-carousel').owlCarousel({
+   center:true,
+   item:3,
+   loop:true,
+   margin:10,
+   nav:false,
+   autoplay:true,
+   autoplayTimeout:2500,
+   dots:false,
+   responsiveClass:true,
+   responsive:{
+   0:{
+       items:1
+   },
+   600:{
+       items:1,
+       nav:true,
+   },
+   762:{
+       items:3
+   }
+   }
+   });
